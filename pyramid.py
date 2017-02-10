dcc58b2816220ded92810278060b1dd6d1aaf112
kronos455 <mbrandon@hotmail.com>
2017-02-10 14:32:06 -0600

    Studying last lecture.

--- pyramid.py
+++ pyramid.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb  4 03:26:17 2017
+Created on Wed Feb  8 14:50:45 2017
 
 @author: bmitchell
 """
 
+
 def pyramid(rows=8):
     for i in range(rows):
-        print ' '*(rows-i-1) + '*'*(2*i+1)
+        print(' '*(rows-i-1) + '*'*(2*i+1))
     
 pyramid()
-    
--- pyramid2.py
+++ pyramid2.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb  4 03:26:17 2017
+Created on Wed Feb  8 14:52:54 2017
 
 @author: bmitchell
 """
 
 def pyramid(rows=8):
     for i in range(rows):
-        print ' '*(rows-i-1) + '*'*(2*i+1)
-    
+        print(' '*(rows-i+6) + '*'*(2*i+6)
+        )
+
 pyramid()
-    
